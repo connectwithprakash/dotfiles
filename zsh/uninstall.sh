e3c406f73fabcf466efbbae116da023ccf597a20
Prakash Chaudhary <connectwithprakash@gmail.com>
2024-06-11 22:59:18 -0500

    [Feat]: Add linux support for zsh uninstall script.

--- zsh/uninstall.sh
+++ zsh/uninstall.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
 
+# Function to check if a command exists
+command_exists() {
+  command -v "$1" >/dev/null 2>&1
+}
+
 # Function to uninstall Oh My Zsh
 uninstall_oh_my_zsh() {
   if [ -d "$HOME/.oh-my-zsh" ]; then
     echo "Uninstalling Oh My Zsh..."
-    rm -rf $HOME/.oh-my-zsh
+    rm -rf "$HOME/.oh-my-zsh"
     echo "Oh My Zsh has been uninstalled."
   else
     echo "Oh My Zsh is not installed."
@@ -12,20 +17,27 @@ uninstall_oh_my_zsh() {
 
   if [ -f "$HOME/.zshrc" ]; then
     echo "Removing .zshrc..."
-    rm -f $HOME/.zshrc
+    rm -f "$HOME/.zshrc"
   fi
   
   if [ -f "$HOME/.p10k.zsh" ]; then
     echo "Removing .p10k.zsh..."
-    rm -f $HOME/.p10k.zsh
+    rm -f "$HOME/.p10k.zsh"
   fi
 }
 
 # Function to uninstall Zsh
 uninstall_zsh() {
-  if command -v zsh &> /dev/null; then
+  if command_exists zsh; then
     echo "Uninstalling Zsh..."
-    brew uninstall zsh
+    if command_exists brew; then
+      brew uninstall zsh
+    elif command_exists apt-get; then
+      sudo apt-get remove --purge -y zsh
+    else
+      echo "Package manager not supported. Please uninstall Zsh manually."
+      exit 1
+    fi
     echo "Zsh has been uninstalled."
   else
     echo "Zsh is not installed."
@@ -36,7 +48,7 @@ uninstall_zsh() {
 change_default_shell() {
   if [ "$SHELL" != "/bin/bash" ]; then
     echo "Changing the default shell back to Bash..."
-    chsh -s /bin/bash $USER
+    chsh -s /bin/bash "$USER"
   else
     echo "Default shell is already Bash."
   fi
@@ -45,21 +57,21 @@ change_default_shell() {
 # Function to remove Zsh-related files and directories
 remove_zsh_files() {
   echo "Removing Zsh-related files and directories..."
-  rm -rf $HOME/.zsh_history
-  rm -rf $HOME/.zshenv
-  rm -rf $HOME/.zlogin
-  rm -rf $HOME/.zlogout
-  rm -rf $HOME/.zsh
-  rm -rf $HOME/.zshrc.d
-  rm -rf $HOME/.cache
+  rm -rf "$HOME/.zsh_history"
+  rm -rf "$HOME/.zshenv"
+  rm -rf "$HOME/.zlogin"
+  rm -rf "$HOME/.zlogout"
+  rm -rf "$HOME/.zsh"
+  rm -rf "$HOME/.zshrc.d"
+  rm -rf "$HOME/.cache"
 }
 
 # Function to remove Zsh plugins and themes directories
 remove_plugins_and_themes() {
   echo "Removing Zsh plugins and themes directories..."
-  rm -rf $HOME/.oh-my-zsh/custom/plugins
-  rm -rf $HOME/.oh-my-zsh/custom/themes
-  rm -rf $HOME/.oh-my-zsh/custom
+  rm -rf "$HOME/.oh-my-zsh/custom/plugins"
+  rm -rf "$HOME/.oh-my-zsh/custom/themes"
+  rm -rf "$HOME/.oh-my-zsh/custom"
 }
 
 # Main function to execute all steps
@@ -75,4 +87,3 @@ main() {
 
 # Run the main function
 main
-
