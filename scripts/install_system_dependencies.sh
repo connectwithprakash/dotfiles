7121bfb6e2a476b5b9c5f102a467eac94ea88f4c
Prakash Chaudhary <connectwithprakash@gmail.com>
2024-07-13 23:46:50 -0500

    [Refactor]: Update system and pipx dependencies installation scripts

--- scripts/install_system_dependencies.sh
+++ scripts/install_system_dependencies.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env bash
 
-# Define a list of dependencies to check and install
-DEPENDENCIES=(
+# Define a list of system dependencies to check and install
+SYSTEM_DEPENDENCIES=(
   "jq"
   "tree"
   "btop"
   "tmux"
   "stats"
-  # Add more dependencies as needed
+  # Add more system dependencies as needed
 )
 
 # Function to check if a command exists
@@ -15,34 +15,39 @@ command_exists() {
   command -v "$1" >/dev/null 2>&1
 }
 
-# Function to install dependencies using Homebrew (for macOS)
-install_with_brew() {
+# Function to install Homebrew (for macOS)
+install_brew() {
   if ! command_exists brew; then
     echo "Homebrew is not installed. Installing Homebrew..."
     /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
     echo "# Setting Homebrew in the PATH" >> "$HOME/.bash_profile"
     echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' >> "$HOME/.bash_profile"
     eval "$(/opt/homebrew/bin/brew shellenv)"
-    source "$HOME/.bash_profile"  # Source the updated profile
+    source "$HOME/.bash_profile"
   fi
-  echo "Installing dependencies using Homebrew..."
+}
+
+# Function to install system dependencies using Homebrew (for macOS)
+install_with_brew() {
+  install_brew
+  echo "Installing $1 using Homebrew..."
   brew install "$1"
 }
 
-# Function to install dependencies using APT (for Debian-based Linux)
+# Function to install system dependencies using APT (for Debian-based Linux)
 install_with_apt() {
   if ! command_exists apt; then
     echo "APT package manager is not available. Please install dependencies manually."
     exit 1
   fi
-  echo "Installing dependencies using APT..."
+  echo "Installing $1 using APT..."
   sudo apt update
   sudo apt install -y "$1"
 }
 
-# Main installation function
-install_dependencies() {
-  for dep in "${DEPENDENCIES[@]}"; do
+# Main function to install system dependencies
+install_system_dependencies() {
+  for dep in "${SYSTEM_DEPENDENCIES[@]}"; do
     if ! command_exists "$dep"; then
       echo "$dep is not installed. Installing..."
       case "$(uname -s)" in
@@ -64,6 +69,6 @@ install_dependencies() {
 }
 
 # Execute the installation function
-install_dependencies
+install_system_dependencies
 
-echo "Dependency installation complete."
+echo "System dependency installation complete."
